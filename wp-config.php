214cf76674352766760cc4ce1efd1cdfd077c4d6
rtroncoso <rod.tronco@gmail.com>
2016-01-26 14:39:49 +0000

    First commit, add wp initial files and structure

---
+++ wp-config.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('REVISR_WORK_TREE', '/sites/rtroncoso.com/public/'); // Added by Revisr
+define('REVISR_GIT_PATH', ''); // Added by Revisr
+define('WP_CACHE', true); //Added by WP-Cache Manager
+define( 'WPCACHEHOME', '/sites/rtroncoso.com/public/wp-content/plugins/wp-super-cache/' ); //Added by WP-Cache Manager
+define('DB_NAME', 'wp_rtroncoso');
+
+/** MySQL database username */
+define('DB_USER', 'wp_rtroncoso');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'wmNuc1$fm');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8mb4');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         ':;X-R=x?K9vmF)Bx7t8|o_kYH%m&Z1e}o&: :/IpHs?023Rq|1XYTYv,uTv4>)Bs');
+define('SECURE_AUTH_KEY',  'O_#igjfVA7qi$4T(;9[o+o*Fi}Mxh.ZcHW+~8~Am(Mpc>45Jr|dn$-<gRx0VJ6:q');
+define('LOGGED_IN_KEY',    '^9v|_EDL)(U-Z6)#;i5bE{?^nfCNDh|-*b*(-p|b=Qrmok%t0Dm>40xM3{0)>,7d');
+define('NONCE_KEY',        '-^ ;|PZLbVw&U`n03[nm)KV>lf).~^a7t.X`e!9 -d}ck6c]Mm]2K35_:;+xI9-=');
+define('AUTH_SALT',        'rs/zdrh1Jx5NY}Cz:?Hdlq]~|&sbudp mk3!C[iXIJ`/?yq?<40`!yh1K5yt= @I');
+define('SECURE_AUTH_SALT', 'G_8~}sXeu95BS~+Dft&fj<c[Fqxz|N::-8rwi0VKS^RU^0LaLY8z+[Xm_SG7Q+gx');
+define('LOGGED_IN_SALT',   'ID|>d#?<+:$B+_QITun4drnn0Y-ECkq&Ne#;.+a{!{|9fh0Xrm8`O+eRBrg_i-|$');
+define('NONCE_SALT',       '!Uq{4yo-F`o(jF%`$,0TA6l>JVCesNEbQJ3m+Tg/,e*e:aL_HcdX$8HtZ}qC3}V)');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define('WP_DEBUG', false);
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
---
+++ wp-content/uploads/revisr-backups/revisr_wp_postmeta.sql
@@ -0,0 +1,40 @@
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+DROP TABLE IF EXISTS `wp_postmeta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `wp_postmeta` (
+  `meta_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `post_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `meta_key` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `meta_value` longtext COLLATE utf8mb4_unicode_ci,
+  PRIMARY KEY (`meta_id`),
+  KEY `post_id` (`post_id`),
+  KEY `meta_key` (`meta_key`(191))
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+LOCK TABLES `wp_postmeta` WRITE;
+/*!40000 ALTER TABLE `wp_postmeta` DISABLE KEYS */;
+INSERT INTO `wp_postmeta` VALUES (1,2,'_wp_page_template','default'),(2,1,'_jetpack_related_posts_cache','a:0:{}'),(3,4,'_wp_attached_file','2016/01/photo-1421790679207-f9ce01b89079.jpeg'),(4,4,'_wp_attachment_metadata','a:4:{s:5:\"width\";i:2508;s:6:\"height\";i:1672;s:4:\"file\";s:45:\"2016/01/photo-1421790679207-f9ce01b89079.jpeg\";s:10:\"image_meta\";a:12:{s:8:\"aperture\";i:0;s:6:\"credit\";s:0:\"\";s:6:\"camera\";s:0:\"\";s:7:\"caption\";s:0:\"\";s:17:\"created_timestamp\";i:0;s:9:\"copyright\";s:0:\"\";s:12:\"focal_length\";i:0;s:3:\"iso\";i:0;s:13:\"shutter_speed\";i:0;s:5:\"title\";s:0:\"\";s:11:\"orientation\";i:0;s:8:\"keywords\";a:0:{}}}'),(5,4,'_wp_attachment_image_alt','Header Image'),(6,5,'_wp_attached_file','2016/01/2_072912_Gaponyuk.jpg'),(7,5,'_wp_attachment_metadata','a:4:{s:5:\"width\";i:1200;s:6:\"height\";i:800;s:4:\"file\";s:29:\"2016/01/2_072912_Gaponyuk.jpg\";s:10:\"image_meta\";a:12:{s:8:\"aperture\";i:0;s:6:\"credit\";s:0:\"\";s:6:\"camera\";s:0:\"\";s:7:\"caption\";s:0:\"\";s:17:\"created_timestamp\";i:0;s:9:\"copyright\";s:0:\"\";s:12:\"focal_length\";i:0;s:3:\"iso\";i:0;s:13:\"shutter_speed\";i:0;s:5:\"title\";s:0:\"\";s:11:\"orientation\";i:1;s:8:\"keywords\";a:0:{}}}'),(8,5,'_wp_attachment_image_alt','China\'s Valleys'),(9,2,'_wp_trash_meta_status','publish'),(10,2,'_wp_trash_meta_time','1453753274'),(11,1,'_edit_lock','1453753411:1');
+/*!40000 ALTER TABLE `wp_postmeta` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
